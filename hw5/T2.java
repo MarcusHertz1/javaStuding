7e210f465db75d230d00d100dc58f162f0ad8214
MarcusHertz1 <111578222+MarcusHertz1@users.noreply.github.com>
2023-03-15 15:05:35 +0100

    add: T2

---
+++ hw5/T2.java
@@ -0,0 +1,15 @@
+package hw5;
+
+/*
+Необходимо вывести на экран числа от 5 до 1.
+(При решении используйте операцию декремента (--).)
+*/
+
+public class T2 {
+    public static void main(String[] args) {
+        for(int i = 5;i > 0; i--) {
+            System.out.print(i + " ");
+
+        }
+    }
+}
