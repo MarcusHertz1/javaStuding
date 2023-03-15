468028bfcf1943f28403fc0259e8cbb868390319
MarcusHertz1 <111578222+MarcusHertz1@users.noreply.github.com>
2023-03-15 15:20:27 +0100

    add: T5

---
+++ hw5/T5.java
@@ -0,0 +1,16 @@
+package hw5;
+
+/*
+Вывести 10 первых чисел последовательности 0, -5,-10,-15
+*/
+
+public class T5 {
+    public static void main(String[] args) {
+        int i = 0, a = 5;
+        while(i != 10){
+            ++i;
+            a -= 5;
+            System.out.print(a + " ");
+        }
+    }
+}
