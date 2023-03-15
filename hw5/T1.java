51dd2f766553d79888f35f9db83323957095d972
MarcusHertz1 <111578222+MarcusHertz1@users.noreply.github.com>
2023-03-15 15:03:20 +0100

    add: hw5 & T1

---
+++ hw5/T1.java
@@ -0,0 +1,16 @@
+package hw5;
+
+/*
+При помощи цикла for вывести на экран нечетные числа от 1 до 99.
+(При решении используйте операцию инкремента (++)).
+*/
+
+public class T1 {
+    public static void main(String[] args) {
+        for(int i = 0;i < 100; i++) {
+            if(i % 2 == 1){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
