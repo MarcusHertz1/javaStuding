d9abda49a775c0986fe9576afe3fa9b6b397554c
MarcusHertz1 <111578222+MarcusHertz1@users.noreply.github.com>
2023-03-15 15:25:19 +0100

    add: T6

---
+++ hw5/T6.java
@@ -0,0 +1,13 @@
+package hw5;
+
+/*
+Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+*/
+
+public class T6 {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 20; i++){
+            System.out.print(i*i + " ");
+        }
+    }
+}
