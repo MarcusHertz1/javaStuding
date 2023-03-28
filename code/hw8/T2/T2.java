49d78a6839187d08a4d0c826232431221e57394e
MarcusHertz1 <111578222+MarcusHertz1@users.noreply.github.com>
2023-03-28 22:52:37 +0200

    fix: T2

--- code/hw8/T2/T2.java
+++ code/hw8/T2/T2.java
@@ -1,4 +1,22 @@
 package code.hw8.T2;
 
+/*
+Написать абстрактный класс Employee.
+От него унаследовать классы "Директор", "Рабочий", "Бухгалтер".
+Реализовать интерфейс с методом, который печатает название должности и
+имплементировать этот метод в созданные классы.
+*/
+
 public class T2 {
+    public static void main(String[] args) {
+        Employee[] positions = {
+                new Director(),
+                new Worker(),
+                new Accountant()
+        };
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Position " + (i + 1) + " = : " + positions[i].printNamePosition());
+        }
+    }
 }
